8341a38eb42fb6bdd5253ad37de4f10722a069d5
Ninetales <matteus.g@hotmail.com>
2016-04-05 13:25:36 +0200

    Updated Login Functionality and added "ForgotPassswordActivity"

--- app/src/main/java/habits/habits/LoginActivity.java
+++ app/src/main/java/habits/habits/LoginActivity.java
@@ -1,7 +1,12 @@
 package habits.habits;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText loginUsername = (EditText) findViewById(R.id.loginUsername);
+        final EditText loginPassword = (EditText) findViewById(R.id.loginPassword);
+        final Button loginBtn = (Button) findViewById(R.id.loginBtn);
+        final TextView forgotPasswordBtn = (TextView) findViewById(R.id.forgotPasswordBtn);
+
+        forgotPasswordBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent registerIntent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+            }
+        });
     }
 }
