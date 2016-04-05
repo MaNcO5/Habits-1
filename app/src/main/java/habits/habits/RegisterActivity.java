f517209b449799acbfcf96bb4c741ccd3dd3a298
Ninetales <matteus.g@hotmail.com>
2016-04-05 13:09:14 +0200

    Updated Register Functionality

--- app/src/main/java/habits/habits/RegisterActivity.java
+++ app/src/main/java/habits/habits/RegisterActivity.java
@@ -2,6 +2,8 @@ package habits.habits;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText registerName = (EditText) findViewById(R.id.registerFname);
+        final EditText registerUsername = (EditText) findViewById(R.id.registerUsername);
+        final EditText registerPassword = (EditText) findViewById(R.id.registerPassword);
+        final EditText registerAge = (EditText) findViewById(R.id.registerAge);
+
+        final Button registerBtn = (Button) findViewById(R.id.registerBtn);
+
+
+
+
     }
 }
