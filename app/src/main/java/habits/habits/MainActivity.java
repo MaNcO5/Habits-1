2a7aa6f138eb49775e84f065d8986d514fc5221b
Ninetales <matteus.g@hotmail.com>
2016-04-05 14:05:34 +0200

    Updated MainActivity - Functionality and Design

--- app/src/main/java/habits/habits/MainActivity.java
+++ app/src/main/java/habits/habits/MainActivity.java
@@ -1,7 +1,10 @@
 package habits.habits;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Login Button
+         * Create connection to button and make it respond to when clicked
+         */
+        final Button mainLoginBtn = (Button) findViewById(R.id.mainSignInBtn);
+
+        mainLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent registerIntent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+
+            }
+        });
+
+        /**
+         * Register Button
+         * Create connection to button and make it respond to when clicked
+         */
+        final Button mainRegisterBtn = (Button) findViewById(R.id.mainRegisterBtn);
+
+
+        mainRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(registerIntent);
+
+            }
+        });
     }
 }
--- app/src/main/java/habits/habits/UserSettings.java
+++ app/src/main/java/habits/habits/UserSettings.java
@@ -2,6 +2,8 @@ package habits.habits;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class UserSettings extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class UserSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+
+        final EditText accountSettingsUsername = (EditText) findViewById(R.id.accountSettingsUsername);
+        final EditText accountSettingsAge = (EditText) findViewById(R.id.accountSettingsAge);
+        final TextView accountSettingsMsg = (TextView) findViewById(R.id.accountSettingsMsg);
     }
 }
