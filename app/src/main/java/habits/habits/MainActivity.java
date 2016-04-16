b4c8cddc098bae18e018e3776fa77c589c5ab373
MaNcO5 <marjansherdenkovski@gmail.com>
2016-04-16 16:10:44 +0200

    New Main(No bars)

--- app/src/main/java/habits/habits/MainActivity.java
+++ app/src/main/java/habits/habits/MainActivity.java
@@ -1,10 +1,14 @@
 package habits.habits;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "Tinier.ttf");
+        TextView myTextView = (TextView)findViewById(R.id.Habits);
+        myTextView.setTypeface(myTypeface);
 
         /**
          * Login Button
